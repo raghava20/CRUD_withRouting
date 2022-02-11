23b364c514a07b1bb3c4469887c73559176d9582
Raghava Anand M <86498250+raghava20@users.noreply.github.com>
2022-02-11 11:18:43 +0530

    Update SideBar.js

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 function SideBar() {
 
     return (
-        <div>
-            <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+        <div className="sidebar">
+            <ul>
                 <a class="sidebar-brand d-flex align-items-center justify-content-center"
                     href="index.html"
                 >
